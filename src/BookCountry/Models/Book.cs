e91933d9f75a18c65dce2d899d07ec58be301267
Maria Romanova <aemulare@users.noreply.github.com>
2017-04-16 23:27:48 -0400

    Creates Book model class and AppDbContext class. Resolves project dependencies.

---
+++ src/BookCountry/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BookCountry.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public string Publisher { get; set; }
+        public string Language { get; set; }
+        public int BindingFormat { get; set; }
+        public string Isbn { get; set; }
+        public decimal Price { get; set; }
+        public string DeweyCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
