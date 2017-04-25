2d7a3561dde0b986a1681b9b00065b3c728d3ee0
Maria Romanova <aemulare@users.noreply.github.com>
2017-04-24 20:19:29 -0400

    Creates class Loan.

---
+++ src/BookCountry/Models/Loan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCountry.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BorrowerId { get; set; }
+        public int BookId { get; set; }
+
+        public DateTime ReservedAt { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public DateTime ReturnedOn { get; set; }
+    }
+}
