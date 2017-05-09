0d3330b23813d63766d478365f390bebf7a12054
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-08 23:59:50 -0400

    Creates LoginViewModel class implementing presentation logic and validation for user login operation.

---
+++ src/BookCountry/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCountry.Models.ViewModels
+{
+    /// <summary>
+    /// Login view model.
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// Email address used as user ID.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Determines whether a login/password should be remembered.
+        /// </summary>
+        [Display(Name="Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
