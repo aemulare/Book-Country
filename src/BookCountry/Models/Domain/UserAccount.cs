d904eb723ffdc8c1b7f1f049f4a1f5d02106feb6
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-08 23:57:57 -0400

    Creates UserAccount class as user domain model.

---
+++ src/BookCountry/Models/Domain/UserAccount.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BookCountry.Models
+{
+    /// <summary>
+    /// A user account domain model.
+    /// </summary>
+    public sealed class UserAccount : IdentityUser
+    {
+    }
+}
