c8f17351edf31d5ccfa5d9645efeef55954cf465
Maria Romanova <aemulare@users.noreply.github.com>
2017-05-09 22:46:11 -0400

    Creates a view model for a new user registration page.

---
+++ src/BookCountry/Models/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCountry.Models.ViewModels
+{
+    /// <summary>
+    /// Registration view model.
+    /// </summary>
+    public sealed class RegistrationViewModel
+    {
+        /// <summary>
+        /// Email address used as user ID.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Password confirmation.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+    }
+}
